c2fcde814dee27c62fde41e3afe945fe31316769
Jacquelyne Glover <jackmidoglover@gmail.com>
2018-11-17 10:52:35 -0700

    worked on portfolio a little

--- ngCourseProject/src/app/app.component.ts
+++ ngCourseProject/src/app/app.component.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 @Component({
-  declarations: [
-    HeaderComponent
-  ],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
